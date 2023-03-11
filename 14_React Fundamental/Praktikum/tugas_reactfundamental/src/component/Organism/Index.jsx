ab3ed6b26235b776c64d473b2e26a90fa8501045
dimasandriano <duangdeong@gmail.com>
2023-03-11 23:57:13 +0700

    add: screenshots

--- 14_React Fundamental/Praktikum/tugas_reactfundamental/src/component/Organism/Index.jsx
+++
@@ -1,3 +0,0 @@
-export { default as Navbar } from "./Navbar/Navbar";
-export { default as ListProduct } from "./List Product/ListProduct";
-export { default as InputProduct } from "./Input Product/InputProduct";
